cfc2e02485971918e2e23998595f4e80c556c6f8
Jude Taylor <me@jude.xyz>
2021-12-24 12:04:01 -0800

    delete everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 #![feature(assert_matches)]
-#![feature(map_first_last)]
-#![feature(option_zip)]
 #![feature(pattern)]
 #![feature(termination_trait_lib)]
 #![feature(trait_alias)]
-#![feature(try_blocks)]
 #![feature(try_trait_v2)]
 
-#[macro_use] extern crate derivative;
 #[macro_use] extern crate derive_more;
 #[macro_use] extern crate enum_as_inner;
 #[macro_use] extern crate lazy_static;
@@ -16,7 +12,5 @@
 #[macro_use] extern crate slog_scope;
 
 pub mod eval;
-pub mod fetch;
-pub mod store;
 pub mod syntax;
 pub mod util;
